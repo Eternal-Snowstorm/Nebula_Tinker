510ef0e14f7248190b4ca8e5727404a0f9f6211e
Qi-Month <dy757410@163.com>
2026-01-11 14:15:26 +0800

    删除冗余导包

--- src/main/java/top/nebula/nebula_tinker/common/register/ModCreativeTab.java
+++ src/main/java/top/nebula/nebula_tinker/common/register/ModCreativeTab.java
@@ -2,12 +2,9 @@ package top.nebula.nebula_tinker.common.register;
 
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.RegistryObject;
 import top.nebula.nebula_tinker.NebulaTinker;
 
 import java.util.function.Supplier;
