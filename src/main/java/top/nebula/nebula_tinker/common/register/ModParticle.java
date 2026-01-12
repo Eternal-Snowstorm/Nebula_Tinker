791b18d34eadebd5197e6e3c860a3ad84cba69f9
Qi-Month <dy757410@163.com>
2026-01-12 23:18:53 +0800

    栗子效果定义失败了..

---
+++ src/main/java/top/nebula/nebula_tinker/common/register/ModParticle.java
@@ -0,0 +1,26 @@
+package top.nebula.nebula_tinker.common.register;
+
+import net.minecraft.core.particles.ParticleType;
+import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import top.nebula.nebula_tinker.NebulaTinker;
+
+import java.util.function.Supplier;
+
+public class ModParticle {
+	public static final DeferredRegister<ParticleType<?>> PARTICLE_TYPES;
+	public static final Supplier<SimpleParticleType> CROSS_CHOP;
+
+	static {
+		PARTICLE_TYPES = DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, NebulaTinker.MODID);
+
+		CROSS_CHOP = register("cross_chop", false);
+	}
+
+	private static Supplier<SimpleParticleType> register(String name, boolean overrideLimiter) {
+		return PARTICLE_TYPES.register(name, () -> {
+			return new SimpleParticleType(overrideLimiter);
+		});
+	}
+}
