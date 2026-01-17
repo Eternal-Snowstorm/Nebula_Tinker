f31364e9a1f30d01288a99cf2151d7b9ec00e131
Eternal-Snowstorm <3512546747@qq.com>
2026-01-17 19:55:22 +0100

    优化"收束"代码

--- src/main/java/top/nebula/nebula_tinker/mixin/ModifiableBowItemMixin.java
+++ src/main/java/top/nebula/nebula_tinker/mixin/ModifiableBowItemMixin.java
@@ -5,11 +5,8 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import slimeknights.tconstruct.library.modifiers.ModifierId;
-import slimeknights.tconstruct.library.tools.helper.ModifierUtil;
 import slimeknights.tconstruct.library.tools.item.ranged.ModifiableBowItem;
 import top.nebula.nebula_tinker.NebulaTinker;
 import top.nebula.nebula_tinker.utils.SimpleTConUtils;
@@ -39,32 +36,21 @@ public class ModifiableBowItemMixin {
 
 		ItemStack main = living.getMainHandItem();
 		ItemStack off = living.getOffhandItem();
-
-		if (nebulaTinker$getConvergeLevel(main) > 3 || nebulaTinker$getConvergeLevel(off) > 3) {
+		int mainHandConvergeLevel = SimpleTConUtils.getModifierLevel(
+				main,
+				NebulaTinker.loadResource("converge").toString());
+		int offhandConvergeLevel = SimpleTConUtils.getModifierLevel(
+				off,
+				NebulaTinker.loadResource("converge").toString());
+		if (mainHandConvergeLevel > 4 || offhandConvergeLevel > 4) {
 			arrow.shootFromRotation(entity, living.getXRot(), yRot, roll, velocity, inaccuracy);
-		} else if (nebulaTinker$getConvergeLevel(main) == 3 || nebulaTinker$getConvergeLevel(off) == 3) {
-			float originalDirection = xRot - living.getXRot();
-			float direction = originalDirection / 4;
-			arrow.shootFromRotation(entity, living.getXRot(), living.getYRot() + direction, roll, velocity, inaccuracy);
-		} else if (nebulaTinker$getConvergeLevel(main) == 2 || nebulaTinker$getConvergeLevel(off) == 2) {
-			float originalDirection = xRot - living.getXRot();
-			float direction = originalDirection / 2;
-			arrow.shootFromRotation(entity, living.getXRot(), living.getYRot() + direction, roll, velocity, inaccuracy);
-		} else if (nebulaTinker$getConvergeLevel(main) == 1 || nebulaTinker$getConvergeLevel(off) == 1) {
-			float originalDirection = xRot - living.getXRot();
-			float direction = 3 * originalDirection / 4;
-			arrow.shootFromRotation(entity, living.getXRot(), living.getYRot() + direction, roll, velocity, inaccuracy);
 		} else {
-			float direction = xRot - living.getXRot();
+			float originalDirection = xRot - living.getXRot();
+
+			int actualConvergence = Math.max(mainHandConvergeLevel, offhandConvergeLevel);
+			float convergedResult = 4 - actualConvergence;
+			float direction = convergedResult * originalDirection / 4;
 			arrow.shootFromRotation(entity, living.getXRot(), living.getYRot() + direction, roll, velocity, inaccuracy);
 		}
-	}
-
-	@Unique
-	private int nebulaTinker$getConvergeLevel(ItemStack stack) {
-		return SimpleTConUtils.getModifierLevel(
-				stack,
-				NebulaTinker.loadResource("converge").toString()
-		);
 	}
 }
