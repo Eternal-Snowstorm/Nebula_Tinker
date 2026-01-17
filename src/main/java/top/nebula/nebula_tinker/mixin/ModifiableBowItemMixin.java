e4bf9cd063687118788b3dc5bc363c802e210858
Qi-Month <dy757410@163.com>
2026-01-17 17:13:53 +0800

    修好了

--- src/main/java/top/nebula/nebula_tinker/mixin/ModifiableBowItemMixin.java
+++ src/main/java/top/nebula/nebula_tinker/mixin/ModifiableBowItemMixin.java
@@ -1,36 +1,49 @@
 package top.nebula.nebula_tinker.mixin;
 
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import slimeknights.tconstruct.library.tools.item.ranged.ModifiableBowItem;
 import top.nebula.nebula_tinker.NebulaTinker;
 import top.nebula.nebula_tinker.utils.SimpleTConUtils;
 
 @Mixin(value = ModifiableBowItem.class, remap = false)
 public class ModifiableBowItemMixin {
-	@ModifyArgs(
+	@Redirect(
 			method = "releaseUsing",
 			at = @At(
 					value = "INVOKE",
 					target = "Lnet/minecraft/world/entity/projectile/AbstractArrow;shootFromRotation(Lnet/minecraft/world/entity/Entity;FFFFF)V"
 			)
 	)
-	public void releaseUsing(Args args) {
-		LivingEntity living = args.get(0);
-		ItemStack bowInMainHand = living.getMainHandItem();
-		ItemStack bowInOffhand = living.getOffhandItem();
-		if (nebulaTinker$isRapidShot(bowInMainHand) || nebulaTinker$isRapidShot(bowInOffhand)) {
-			args.set(1, living.getXRot());
+	private void nebulaTinker$redirectShootFromRotation(
+			AbstractArrow arrow,
+			Entity shooter,
+			float xRot,
+			float yRot,
+			float roll,
+			float velocity,
+			float inaccuracy
+	) {
+		if (!(shooter instanceof LivingEntity living)) {
+			arrow.shootFromRotation(shooter, xRot, yRot, roll, velocity, inaccuracy);
+			return;
+		}
+
+		ItemStack main = living.getMainHandItem();
+		ItemStack off = living.getOffhandItem();
+
+		if (nebulaTinker$isRapidShot(main) || nebulaTinker$isRapidShot(off)) {
+			// 你想要的快速射击逻辑
+			arrow.shootFromRotation(shooter, living.getXRot(), yRot, roll, velocity, inaccuracy);
 		} else {
-			float spread = args.get(1);
-			float direction = spread - living.getXRot();
-			args.set(1, living.getXRot());
-			args.set(2, living.getYRot() + direction);
+			float direction = xRot - living.getXRot();
+			arrow.shootFromRotation(shooter, living.getXRot(), living.getYRot() + direction, roll, velocity, inaccuracy);
 		}
 	}
 
