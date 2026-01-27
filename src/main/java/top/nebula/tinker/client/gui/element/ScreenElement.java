1df2c95da3bef7ce8275e46c33f2a0a15fc30224
Qi-Month <dy757410@163.com>
2026-01-27 17:50:23 +0800

    util

---
+++ src/main/java/top/nebula/tinker/client/gui/element/ScreenElement.java
@@ -0,0 +1,10 @@
+package top.nebula.tinker.client.gui.element;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+public interface ScreenElement {
+	@OnlyIn(Dist.CLIENT)
+	void render(GuiGraphics var1, int var2, int var3);
+}
